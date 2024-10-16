505284084e7dfe39bfa7458243c1935cb4c0923c
merve <mervealtinisik99@gmail.com>
2024-10-16 13:21:14 +0300

    Finish

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import news_data from "./news_data.json"
 import NewsCard from "./components/NewsCard"
 import news_banner_data from "./news_banner_data.json"
 function App(){             //FlatList mobile uygulamamda yoğun bir veri kullanmak istediğimde kullanacağım component, ekrana ne kadar çok veri kullanırsak     kullanalım uygulamayı yormadan kontrollu bir şekilde kullanıcı deneyimi için tasarlanmış özel bir module
+
+
+  const renderNews = ({item}) => <NewsCard haberler={item}/>        //Bu şekilde fonksiyonları dışarıda tanımlarsak örneğin 200 kere fonksiyon oluşturmak yerine 1 kere oluşturup gerektiğinde bunu çağıracak 
+  const keyExtractorFuunction = item => item.u_id.toString()
+
   return(
   
     <View style ={styles.container}>
-      <View>
+     <Text style ={styles.headerText}>News</Text>
+
         <FlatList
+        ListHeaderComponent={() => (        
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {
+              news_banner_data.map(bannerNews => (
+              <Image 
+              style={styles.banner_image}
+              source={{uri: bannerNews.imageUrl}}
+              />
+            ))
+            }
+          </ScrollView>
+        )}
+        keyExtractor={keyExtractorFuunction}
         data={news_data}
-        renderItem={({item}) => <NewsCard haberler={item}/>}
+        renderItem={renderNews}
         />
-      </View> 
     </View>
  
   )
@@ -22,8 +40,24 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
     backgroundColor: '#eceff1',
+  },
+  
+  banner_image:{
+    height: Dimensions.get('window').height / 5,
+    width: Dimensions.get('window').width / 2,
+    margin :1,
+    borderRadius: 5,
+    
+  },
+  headerText:{
+    fontSize: 50,
+    fontWeight: 'bold',
+    paddingLeft: 10,
   }
+
+  
 })
+
 
 
   export default App;
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -3,11 +3,17 @@ import{View, Text, Image} from 'react-native';
 import styles from './NewsCard.style';
 
 const NewsCard = ({haberler}) => {return(
-    <View style={Styles.container}>
-        < Image style={Styles.image}  source={{uri:haberler.imageUrl}}/>
+    <View style={styles.container}>
+        < Image style={styles.image}  source={{uri:haberler.imageUrl}}/>
+
+        <View style={styles.inner_container}> 
+
         <Text style={styles.title}> {haberler.title} </Text>
         <Text style={styles.description}> {haberler.description} </Text>
+        <Text style={styles.author}> {haberler.author} </Text>
 
+        </View>
+    
     </View>
 )}
 
